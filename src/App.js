03bc6e4e65b7a0f93868c491addf9947e78da222
Vishant Dhiman <dman.vis94@gmail.com>
2021-10-10 21:49:17 +0530

    Render Array on homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomBox from './Box.js';
+import BoxContainer from './BoxContainer.js';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <BoxContainer/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
 }
 
 export default App;
