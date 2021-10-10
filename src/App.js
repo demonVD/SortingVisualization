957bee79c5cf2b9566eb244ba1b296f7d9391579
Vishant Dhiman <dman.vis94@gmail.com>
2021-10-10 22:08:40 +0530

    Maintain state in BoxContainer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import CustomBox from './Box.js';
 import BoxContainer from './BoxContainer.js';
 
+
+const array = [5, 4, 3, 2, 1];
+
 function App() {
     return (
-        <div className="App"><BoxContainer/></div>
+        <div className="App"><BoxContainer array={array}/></div>
     );
 }
 
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -2,12 +2,20 @@ import React from 'react';
 import CustomBox from './Box.js';
 import './BoxContainer.css';
 
-const array = [5, 4, 3, 2, 1];
-const listItems = array.map((number) => <CustomBox number={number}/>);
+
+
+
+// const array = [5, 4, 3, 2, 1];
+// const listItems = array.map((number) => <CustomBox number={number}/>);
 
 class BoxContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { array: props.array };
+    }
     render() {
-        return (<div className='flexbox-container'>{ listItems }</div>);
+        return (<div className='flexbox-container'>{ this.state.array.map((number) => <CustomBox number={number}/>)}</div>);
     }
 }
 
